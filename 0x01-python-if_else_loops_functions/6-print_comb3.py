e5b9aeb2c922be0fa8300038179dd41a9e019ff4
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-06-01 04:13:16 +0100

    loops and condition

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for n in range(0, 9):
+    for i in range(n + 1, 10):
+        if n == 8 and i == 9:
+            print("{:d}{:d}".format(n,i))
+        else:
+            print("{:d}{:d}".format(n, i), end=", ")
