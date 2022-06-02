30d919a8110c152c01ae2ba081913a369e663eaf
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-06-02 18:46:30 +0100

    import and modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import sys
+    n = len(sys.argv) - 1
+    if n == 0:
+        print("0 arguments")
+    elif n == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(n))
+    for i in range(n):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
