790cb1ac35ec963c662773e9610292d975ee9689
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-06-05 02:11:22 +0100

    lists and tuple

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""program replaces an element in a list at specific position"""
+
+def replace_in_list(my_list, idx, element):
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
+    return(my_list)
