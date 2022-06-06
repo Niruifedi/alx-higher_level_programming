252fce767c7a58496a91a70d9d76bac39b1c1e13
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-06-06 14:57:17 +0100

    lists and tuples

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" function prints all multiples of 2 in a list"""
+
+
+def divisible_by_2(my_list=[]):
+    booll = my_list[:]
+    for count, n in enumerate(my_list):
+        if n % 2 == 0:
+            booll[count] = True
+        else:
+            booll[count] = False
+    return(booll)
+            
