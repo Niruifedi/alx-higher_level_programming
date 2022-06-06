428d0add5271a883cf44e6ed5c9e895bdde7de24
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-06-06 14:30:57 +0100

    lists and tuples

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,10 +4,10 @@
 
 
 def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return ("None")
     big_n = 0
     for n in my_list:
-        if my_list  == 0:
-            return "None"
         if n > big_n:
             big_n = n
     return (big_n)
