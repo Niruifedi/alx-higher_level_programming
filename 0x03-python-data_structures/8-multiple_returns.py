15776e22c736c253d58950f00c7a1b4846cbf40b
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-06-06 14:06:41 +0100

    list andtuples

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""function returns length of tuple characters and its first character"""
+
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    f_s = sentence[0]
+    
+    return(length, f_s)
