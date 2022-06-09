3004e53b896bbed176fd9c00fd3fe88abc53f67f
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-06-09 01:22:01 +0100

    max value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,6 +4,6 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == {}:
+    if not a_dictionary:
         return None
-    return max(a_dictionary)
+    return max(a_dictionary, key=a_dictionary.get)
