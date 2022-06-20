d690277405ef9c41903c52b548f607d1aaa87647
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-06-21 00:42:30 +0100

    exception

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        func = fct(*args)
+        return func
+    except Exception as e:
+        print('Exception: {}'.format(e), file=sys.stderr)
