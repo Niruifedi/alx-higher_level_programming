ca503be935f92dea1c6f2babb9dc62a5cfd74303
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-07-05 20:31:22 +0100

    to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Json String"""
+import json
+
+
+def to_json_string(my_obj):
+    """function returns a Json representation
+        of a string
+    """
+    return json.dumps(my_obj)
