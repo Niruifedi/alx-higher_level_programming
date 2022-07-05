69d32dac5d47055a7ead6c62599a11ce5ead4685
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-07-05 22:41:13 +0100

    arguments

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,11 +9,10 @@ if __name__ == '__main__':
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
-    
+
     try:
         new_list = load_from_json_file('add_item.json')
     except FileNotFoundError:
-            new_list = []
+        new_list = []
     new_list.extend(argv[1:])
     save_to_json_file(new_list, 'add_item.json')
-    
