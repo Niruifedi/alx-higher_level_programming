700fd5163e80491a45b7d698fe1d7d66c2dda7c4
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-07-05 22:40:12 +0100

    arguments

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,16 +4,16 @@
 """
 
 from sys import argv
-import json
-
 if __name__ == '__main__':
 
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-    new_list = []
-    file_name = "add_item.json"
-
-    for i in argv[1:]:
-        new_list.append(i)
-    save_to_json_file(new_list, file_name)
-    load_from_json_file(file_name)
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+    
+    try:
+        new_list = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+            new_list = []
+    new_list.extend(argv[1:])
+    save_to_json_file(new_list, 'add_item.json')
+    
