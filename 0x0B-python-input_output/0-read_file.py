68384628819ccd616bc93e1a0d043b5aea01a9af
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-07-05 13:59:27 +0100

    input & output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""funtion to read file"""
+
+def read_file(filename=""):
+    """function reads out file name 
+        and prints to stdout.
+        Args:
+        filename:
+        file to read from
+    """
+    with open(filename, 'r', encoding='UTF-8') as f:
+        print(f.read(), end="")
