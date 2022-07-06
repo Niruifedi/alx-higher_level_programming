c6610a4e6c047125bb83654f523b359a57cadd27
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-07-06 02:15:47 +0100

    json with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Student to Json filter"""
+
+
+class Student:
+    """class defines student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves dictionaryu representation
+            If attrs is a list of strings, represents only those attributes
+        included in the list.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
