9b777a8b122c5cdddfa44f5189ebc5c95e3d1ecb
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-07-08 22:58:22 +0100

    circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Base Class."""
+
+
+class Base():
+    """class Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """instantiation constructor
+            id: can only accept integer value
+        """
+        if id != None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = Base.__nb_objects
