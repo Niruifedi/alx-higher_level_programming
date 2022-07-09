fdb70933b299e5b57d8f826c75fbd0fa29948a49
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-07-09 22:30:43 +0100

    log

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,7 +3,9 @@ import sys
 
 
 def print_status():
-    """print status from stdin"""
+    """
+        print status from stdin
+    """
     size = 0
     counter = 0
     status_code = {'200': 0, '301': 0, '400': 0, '405': 0,
@@ -27,10 +29,11 @@ def print_status():
             counter = 0
         counter += 1
     if counter < 9:
-         print("File size: {}".format(size))
-         for key, val in status_code.items():
-                if val != 0:
-                    print("{}: {}".format(key, val))
+        print("File size: {}".format(size))
+        for key, val in status_code.items():
+            if val != 0:
+                print("{}: {}".format(key, val))
+
 
 if __name__ == '__main__':
     print_status()
