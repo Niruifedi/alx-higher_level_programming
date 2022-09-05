4fa58659fe4b362cdb4196f85d4d68d53cac5d24
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-09-05 21:20:01 +0100

    arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const myArgs = process.argv;
+
+if (myArgs.length === 2) {
+  console.log('No argument');
+} else if (myArgs.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
