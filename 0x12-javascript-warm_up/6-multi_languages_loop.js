5af40436bb3b6fb3070e7e8e6b059def7ee2ef8a
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-09-06 07:36:54 +0100

    loops

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let new_arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const new_arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < new_arr.length; i++) {
   console.log(new_arr[i]);
