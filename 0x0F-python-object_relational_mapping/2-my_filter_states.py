fccfe4cfef5130e683b08b6dd21f1d568195352d
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-09-11 23:32:10 +0100

    orm

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script that takes an argument
+Displays all the values in states
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(
+        host='localhost', port=3306,
+        user=argv[1], password=argv[2],
+        database=argv[3]
+    )
+    cur = con.cursor()
+cur.execute("SELECT * FROM states WHERE name LIKE \
+            '{:s}';".format(argv[4]))
+db = cur.fetchall()
+
+for i in db:
+    print(i)
