b8aad33d6103e01267d23c5881d0508f99166b29
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-09-13 00:37:08 +0100

    orm

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,16 +8,22 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
+    cont = 0
     con = MySQLdb.connect(
         host='localhost', port=3306,
         user=argv[1], password=argv[2],
-        database=argv[3]
+        database=argv[3], charset="utf8"
     )
     cur = con.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
             FROM cities JOIN states ON cities.state_id = states.id \
-            WHERE states.name = '{}';".format(argv[4],))
-    db = cur.fetchall()
-
-    for i in db:
-        print(i)
+            WHERE states.name = '{}';".format(argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    #     if row[2] == argv[4]:
+    #         if cont > 0:
+    #             print(", ", end="")
+    #         print(row[1], end="")
+    #         cont = cont + 1
+    # print()
