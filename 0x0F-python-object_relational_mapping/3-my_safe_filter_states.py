52797fa14f0c13773d0d0525b3e2c92e7d3ee650
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-09-12 09:21:24 +0100

    orm

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Script diplays content of a table 
+This is a more secure script against SQL injection
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    con = MySQLdb.connect(
+        host='localhost', port=3306,
+        user=argv[1], password=argv[2],
+        database=argv[3]
+    )
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",(argv[4],))
+    db = cur.fetchall()
+
+    for i in db:
+        print(i)
