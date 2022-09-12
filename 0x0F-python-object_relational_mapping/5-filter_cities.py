c953dfab372242bb08a4f796ef3f1ea2073c5cf7
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-09-12 18:33:06 +0100

    orm

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     cur = con.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
             FROM cities JOIN states ON cities.state_id = states.id \
-            WHERE states.name = '{}';".format(argv[4]))
+            WHERE states.name = '{}';".format(argv[4],))
     db = cur.fetchall()
 
     for i in db:
