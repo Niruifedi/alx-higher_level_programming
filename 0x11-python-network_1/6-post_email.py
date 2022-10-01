8f9fe2b42a04d7982a1df26ceec3918f2b59cc19
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-10-01 13:13:51 +0100

    netwrok

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL and an email address, sends a POST request to the passed URL
-with the email as a parameter, and finally displays the body of the response
+Script that takes in a URL and an email address,
+sends a POST request to the passed URL
+with the email as a parameter,
+and finally displays the body of the response
 """
 import requests
 from sys import argv
