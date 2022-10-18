295bbc7debf969eca4f0de2016b79f1cfbdcb0c8
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-10-18 22:15:58 +0100

    fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 // A script that display the status code of a GET request
 
 const args = process.argv;
-let request = require('request');
+const request = require('request');
 request(args[2], function (error, response, body) {
   if (error) {
     console.log('error:', error); // Print the error if one occurred
